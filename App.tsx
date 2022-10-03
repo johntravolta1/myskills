36a9eeea51e0f2bf86b1daf3fb184d1d6099559c
John Travolta <m3u0v0@gmail.com>
2022-10-03 16:44:09 -0300

    fim do curso

--- App.tsx
+++ App.tsx
@@ -3,6 +3,15 @@ import { Home } from './src/pages/Home'
 import {StatusBar} from 'react-native'
 import SplashScreen from 'react-native-splash-screen'
 import codePush from "react-native-code-push";
+import * as Sentry from "@sentry/react-native";
+
+
+Sentry.init({
+  dsn: "https://6dd7476ba73a48c6b95ed80a10c78e69@o1249513.ingest.sentry.io/4503917582876672",
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  // We recommend adjusting this value in production.
+  tracesSampleRate: 1.0,
+});
 
 function App() {
 
@@ -10,6 +19,8 @@ function App() {
     codePush.sync({
       installMode: codePush.InstallMode.IMMEDIATE
   });
+
+    // throw new Error('Não foi possível abrir a aplicação. Tente mais tarde!')
 
     SplashScreen.hide()
   } ,[])
